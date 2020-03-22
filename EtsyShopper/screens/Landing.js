292a4fc21733a736ca201aa81ac2b296e8074cef
Annie Zhang <azhang@quartermaster.house>
2020-03-22 18:46:34 -0400

    Add lottie animation, add custom button component

--- EtsyShopper/screens/Landing.js
+++ EtsyShopper/screens/Landing.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import {SafeAreaView, Button} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import LottieView from 'lottie-react-native';
 
+/** Components */
 import Text from '../components/Text';
+import Button from '../components/Button';
+
+/** Styles */
+import {deviceWidth, baseSpacing} from '../styles/defaultStyles';
 
 const Landing = ({navigation}) => {
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#fff',
         alignItems: 'center',
-        alignContent: 'center',
       }}>
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Landing Text</Text>
-      <Text style={{fontSize: 16}}>Some text about the app here</Text>
-      <Button
-        title="Start"
-        color="green"
-        onPress={() => {
-          navigation.navigate('UserLocation');
-        }}
-      />
+      <View style={{flex: 1}}>
+        <LottieView
+          ref={animation => {
+            this.animation = animation;
+          }}
+          style={{width: 300, maxWidth: deviceWidth - baseSpacing * 2}}
+          autoPlay
+          source={require('../animations/welcome.json')}
+        />
+      </View>
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Text style={{fontSize: 28, fontWeight: 'bold'}}>Hello!</Text>
+        <Text style={{fontSize: 16}}>
+          Shop and support your local small businesses.
+        </Text>
+        <Button
+          title="Get Started"
+          backgroundColor="#fac472"
+          onPress={() => {
+            navigation.navigate('UserLocation');
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
