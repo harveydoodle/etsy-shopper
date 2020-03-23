97eed18cc68570f5f40a9aed8ec7648fc4d300f7
Annie Zhang <azhang@quartermaster.house>
2020-03-23 19:27:47 -0400

    Refactor styles

--- EtsyShopper/components/Text.js
+++ EtsyShopper/components/Text.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 
 import {baseFontColor} from '../styles/defaultStyles';
 
 const CustomText = props => (
-  <Text
-    {...props}
-    style={{
-      fontFamily: 'futura',
-      fontSize: 16,
-      color: baseFontColor,
-      ...props.style,
-    }}>
+  <Text {...props} style={{...styles.text, ...props.style}}>
     {props.children}
   </Text>
 );
 
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'futura',
+    fontSize: 16,
+    color: baseFontColor,
+  },
+});
 export default CustomText;
